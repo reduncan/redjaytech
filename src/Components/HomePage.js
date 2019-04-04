752e8605917ebc29f0d4323b43ba2e93d149adad
rduncan <rduncan@phpatlanta.com>
2019-04-03 21:11:31 -0400

    styling of homepage

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -9,45 +9,96 @@ import MenuIcon from '@material-ui/icons/Menu';
 
 const styles = theme => ({
     root: {
-        backgroundColor: 'rgb(35, 35, 35)'
+        backgroundColor: 'rgb(35, 35, 35)',
+        height: "100vh"
     },
     main: {
         marginTop: 0,
-        minHeight: 740,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center center'
+        height: "100%",
+        backgroundImage: 'url("/redjay-light.png")',
+        backgroundAttachment: "fixed",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover"
     },
     appBar: {
         backgroundColor: "transparent !important",
-        boxShadow: "none"
+        boxShadow: "none",
+        position: "fixed"
     },
     icon: {
-        color: 'white'
+        color: 'rgb(237, 32, 36)'
+    },
+    whoWeAreCon: {
+        color: 'rgb(35, 35, 35)',
+        background: 'rgb(255, 255, 255)',
+        height: 200,
+        textAlign: 'right'
+    },
+    whatWeDo: {
+        height: 350,
+        backgroundImage: 'url("/what-we-do.jpg")',
+        backgroundAttachment: "fixed",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover"
+    },
+    whatWeDoCon: {
+        color: 'rgb(35, 35, 35)',
+        background: 'rgb(255, 255, 255)',
+        height: 200,
+        textAlign: 'right',
+    },
+    ourExperienceCon: {
+        color: 'rgb(35, 35, 35)',
+        background: 'rgb(255, 255, 255)',
+        height: 200,
+        textAlign: 'right'
     }
 });
-
-const style = {
-    backgroundImage: 'url("/redjay-light.png")'
-}
 
 function HomePage(props) {
 
     const { classes } = props;
 
     return (
-        <React.Fragment>
-            <Grid container className={classes.root}>
-                <Grid item xs={12} style={style} className={classes.main}>
-                    <AppBar className={classes.appBar}>
-                        <Toolbar>
-                            <IconButton aria-label="Menu" className={classes.icon}>
-                                <MenuIcon />
-                            </IconButton>
-                        </Toolbar>
-                    </AppBar>
+        <div className={classes.root}>
+            <AppBar className={classes.appBar}>
+                <Toolbar>
+                    <IconButton aria-label="Menu" className={classes.icon}>
+                        <MenuIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <div className={classes.main}></div>
+            <Grid container>
+                <Grid item xs={12} sm={12} md={6} className={classes.whoWeAreCon}>
+                    <h1>WHO WE ARE</h1>
+                </Grid>
+                <Grid item xs={12} sm={12} md={6} className={classes.whoWeAreCon}>
+                    <p>Bacon ipsum dolor amet picanha tongue meatball buffalo kevin strip steak, ground round sausage beef ribs pancetta tail flank. Meatloaf pork chop pork belly swine pastrami cow. Bresaola jowl ham andouille. Corned beef chuck ham strip steak jerky pork loin salami, boudin brisket kevin landjaeger. Beef shoulder tenderloin, biltong ribeye frankfurter strip steak drumstick. Tongue shoulder filet mignon, drumstick leberkas kielbasa beef ribs cupim corned beef bresaola.</p>
+                    <p>Bacon ipsum dolor amet doner turkey ball tip, shank venison ground round pig picanha pork belly biltong sausage swine brisket jowl. Kevin capicola short loin venison burgdoggen pig shank swine landjaeger buffalo chuck. Rump turkey shank burgdoggen ball tip tongue short loin hamburger. Ribeye pig flank short ribs jerky. Short ribs ball tip burgdoggen landjaeger, jerky andouille pastrami jowl boudin doner filet mignon turducken hamburger porchetta. Chuck capicola cupim biltong. Jowl buffalo fatback swine cow capicola ribeye pastrami biltong.</p>
                 </Grid>
             </Grid>
-        </React.Fragment>
+            <div className={classes.whatWeDo}></div>
+            <Grid container>
+                <Grid item xs={12} sm={12} md={6} className={classes.whatWeDoCon}>
+                    <h2>WHAT WE DO</h2>
+                </Grid>
+                <Grid item xs={12} sm={12} md={6} className={classes.whatWeDoCon}>
+                <p>Ground round flank tri-tip pork chop buffalo bacon hamburger, fatback ham hock. Boudin venison pancetta tongue pork shank picanha meatball porchetta landjaeger. Tongue beef chuck prosciutto, picanha tri-tip leberkas hamburger flank. Bresaola sausage turkey turducken pastrami. Shankle beef ribs flank alcatra bacon leberkas pork belly venison spare ribs prosciutto. T-bone swine shoulder kielbasa tenderloin. Biltong chuck meatball pancetta alcatra pork chop.</p>
+                </Grid>
+            </Grid>
+            <div className={classes.whatWeDo}></div>
+            <Grid container>
+                <Grid item xs={12} sm={12} md={6} className={classes.ourExperienceCon}>
+                    <h2>OUR EXPERIENCE</h2>
+                </Grid>
+                <Grid item xs={12} sm={12} md={6} className={classes.ourExperienceCon}>
+                <p>Buffalo biltong ribeye ham, alcatra pastrami bresaola cupim cow. Ham shoulder porchetta turkey kielbasa, ground round flank meatloaf pork ribeye ball tip tri-tip. Shankle ham hock tongue kevin filet mignon pork belly prosciutto buffalo sausage hamburger landjaeger turducken turkey doner rump. T-bone bacon pork belly frankfurter shank meatloaf ribeye. Pancetta sausage venison bacon meatloaf picanha rump drumstick tongue andouille. Jerky landjaeger brisket, ground round t-bone ribeye andouille pancetta fatback jowl capicola corned beef pastrami pork belly ball tip.</p>
+                </Grid>
+            </Grid>
+        </div >
     )
 };
 
